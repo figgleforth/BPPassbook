367249128be916301ab903445b0c10a6646e7d93
Bojan Percevic <percevic@me.com>
2012-09-17 12:19:44 -0400

    Initial Commit

---
+++ Passbook/BPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BPAppDelegate.h
+//  Passbook
+//
+//  Created by Bojan Percevic on 9/17/12.
+//  Copyright (c) 2012 Bojan Percevic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Passbook/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Passbook
+//
+//  Created by Bojan Percevic on 9/17/12.
+//  Copyright (c) 2012 Bojan Percevic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BPAppDelegate class]));
+    }
+}
